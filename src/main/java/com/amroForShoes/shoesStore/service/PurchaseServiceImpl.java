f6fefd067df20cedd050aaa95886d84e711dea4e
Amro Abdalla <wed.kom@hotmail.com>
2022-06-14 13:35:55 -0500

    additional implementation

--- src/main/java/com/amroForShoes/shoesStore/service/PurchaseServiceImpl.java
+++ src/main/java/com/amroForShoes/shoesStore/service/PurchaseServiceImpl.java
@@ -3,6 +3,7 @@ package com.amroForShoes.shoesStore.service;
 import com.amroForShoes.shoesStore.entites.Purchase;
 import com.amroForShoes.shoesStore.repository.PurchaseRepository;
 import com.amroForShoes.shoesStore.repository.projection.PurchaseItem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,7 +12,7 @@ import java.util.List;
 @Service
 public class PurchaseServiceImpl implements PurchaseService {
 
-
+    //@Autowired
     private PurchaseRepository purchaseRepository;
 
     public PurchaseServiceImpl(PurchaseRepository purchaseRepository) {
